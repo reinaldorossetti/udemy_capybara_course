a7894a7f83473fe8f3697ec8287fddb34d63b155
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2018-09-22 10:06:28 -0300

    Update send_by_char.rb

--- exemplos/send_by_char.rb
+++ exemplos/send_by_char.rb
@@ -1,16 +1,15 @@
 require 'capybara'
-#require 'selenium-webdriver'
 
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, :browser => :chrome)
 end
 
 Capybara.default_max_wait_time = 30
-$session = Capybara::Session.new(:chrome)
-$session.visit("https://erp1.edebe.com.br:8085/?StartProg=SIGAMDI&Comm=TCP&Env=P12_PROD")
+page = Capybara::Session.new(:chrome)
+page.visit("https://erp1.edebe.com.br:8085/?StartProg=SIGAMDI&Comm=TCP&Env=P12_PROD")
 
-elem01 = $session.find('#COMP3014 > input').set "reinaldo"
-elem02 = $session.find(:xpath, "//*[@id='COMP3016']/input")
+elem01 = page.find('#COMP3014 > input').set "reinaldo"
+elem02 = page.find(:xpath, "//*[@id='COMP3016']/input")
 " help".each_char{|letra|elem02.send_keys letra
 sleep 0.5}
 sleep 5
